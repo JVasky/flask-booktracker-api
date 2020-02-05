f6375f336c1490c88ca92215a972258c4bd90733
Jim Vasky <vaskyj2@gmail.com>
2020-02-04 21:02:49 -0500

    Add LowerCaseText TypeDecorator to user model. Add role and user_roles tables/relationship

--- app/models/users.py
+++ app/models/users.py
@@ -1,17 +1,50 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+
+
+class LowerCaseText(db.TypeDecorator):
+    # Converts strings to lower case
+
+    impl = db.String(30)
+
+    def process_bind_param(self, value, dialect):
+        return value.lower()
 
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
+    username = db.Column(LowerCaseText, unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     first_name = db.Column(db.String(30), index=True, nullable=False)
     last_name = db.Column(db.String(30), index=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    roles = relationship('Role', secondary='user_roles')
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Role(db.Model):
+    __tablename__ = "role"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    users = relationship(User, secondary='user_roles')
+
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey(User.id),
+        primary_key=True
+    )
+    role_id = db.Column(
+        db.Integer,
+        db.ForeignKey(Role.id),
+        primary_key=True
+    )
