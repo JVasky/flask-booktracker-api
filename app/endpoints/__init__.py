6a009aedfa64f5660bd6cc87259a288e7d7cddff
James Vasky <vaskyj2@gmail.com>
2020-02-05 12:03:44 -0500

    Add custom 404 handler to return JSON

--- app/endpoints/__init__.py
+++ app/endpoints/__init__.py
@@ -1,4 +1,12 @@
 def init_app(app):
+    from . import authentication
     from . import authors
     from . import books
     from . import users
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        # set the 404 status explicitly
+        return {
+            "message": "Not found"
+        }, 404
